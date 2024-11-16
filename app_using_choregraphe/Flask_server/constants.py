a365f1a8c1b5f15eec5d1918c8a57cb936647245
Nicolò Sansevrino <n.sansevrino@campus.unimib.it>
2024-11-16 17:59:51 +0100

    filled constats.py

--- app_using_choregraphe/Flask_server/constants.py
+++ app_using_choregraphe/Flask_server/constants.py
@@ -1,6 +1,30 @@
 poses = {
+    "chair":{
+        "image_path": "chair.jpg"
+    },
+    "child":{
+        "image_path": "child.jpg"
+    },
+    "cobra":{
+        "image_path": "cobra.jpg"
+    },
+    "half_standing_forward_bend":{
+        "image_path": "half_standing_forward_bend.jpg"
+    },
+    "legs_up_the_wall":{
+        "image_path": "legs_up_the_wall.jpg"
+    },
+    "low_lunge":{
+        "image_path": "low_lunge.jpg"
+    },
+    "staff":{
+        "image_path": "staff.jpg"
+    },
     "warrior": {
         "image_path": "warrior.jpg",
+    },
+    "warrior_2": {
+        "image_path": "warrior_2.jpg",
     },
     "tadasana": {
         "image_path": "tadasana.jpg",
