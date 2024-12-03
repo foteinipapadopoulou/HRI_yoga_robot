6e49f634ac3e930abef6e04be12c32e9b1e34dc7
LDGerrits <loekgerrits@gmail.com>
2024-12-03 17:39:08 +0100

    Fix image_path and make ip address handling more generic

--- app_using_choregraphe/Flask_server/main.py
+++ app_using_choregraphe/Flask_server/main.py
@@ -4,32 +4,53 @@ import os
 
 from yoga_feedback import get_feedback 
 
+current_directory = os.path.dirname(os.path.abspath(__file__))
+
 app = Flask(__name__)
 
 @app.route('/get_yoga_feedback', methods=['POST'])
 def get_yoga_feedback():
-    try:
-        pose_name = request.json.get('pose_name')
-        image_data = request.json.get('image_data')
-        
-        image_bytes = base64.b64decode(image_data)
-        
-        #save the image
-        with open("received_image.jpg", "wb") as image_file:
-            image_file.write(image_bytes)
+	try:
+		pose_name = request.json.get('pose_name')
+		image_data = request.json.get('image_data')
 
-        #give feedback
-        message = get_feedback("received_image.jpg", pose_name)
+		print(f"Received pose_name: {pose_name}")
+		print(f"Received image data: {image_data[:100]}...")
+		
+		image_bytes = base64.b64decode(image_data)
 
-        #here we can add something like llm to fix the message feedback
+		image_path = os.path.join(current_directory, 'received_image.jpg')
 
-        #remove image
-        os.remove("received_image.jpg")
-        print(message)
-        return jsonify(message=message), 200
-    except Exception as e:
-        print(str(e))
-        return jsonify(error=str(e)), 400
+		#save the image
+		with open(image_path, 'wb') as image_file:
+			image_file.write(image_bytes)
+
+		#give feedback
+		message = get_feedback(image_path, pose_name)
+
+		#here we can add something like llm to fix the message feedback
+		
+		#remove image
+		os.remove(image_path)
+
+		return jsonify(message=message), 200
+	except Exception as e:
+		print(str(e))
+		return jsonify(error=str(e)), 400
+
+def get_server_config():
+	try:
+		config_file_path = os.path.join(current_directory, 'server_config.txt')
+		with open(config_file_path, 'r') as config_file:
+			config_line = config_file.readline().strip()
+			ip_address, port = config_line.split(':')
+			port = int(port)
+			return ip_address, port
+	except Exception as e:
+		print(f"Error reading the config file: {str(e)}")
+		return None, None
 
 if __name__ == '__main__':
-    app.run(debug=True)
+	ip_address, port = get_server_config()
+	if ip_address and port:
+		app.run(host=ip_address, port=port, debug=True)
