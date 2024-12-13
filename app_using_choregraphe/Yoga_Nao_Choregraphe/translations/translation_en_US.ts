5ab5b010b50e80006e75ebadb03a36a48708fac2
LDGerrits <loekgerrits@gmail.com>
2024-12-13 15:46:18 +0100

    Add rest of reversed poses

--- app_using_choregraphe/Yoga_Nao_Choregraphe/translations/translation_en_US.ts
+++ app_using_choregraphe/Yoga_Nao_Choregraphe/translations/translation_en_US.ts
@@ -72,10 +72,17 @@
     <context>
         <name>behavior_with_feedback/behavior.xar:/Ardha Uttanasana (1)/Say</name>
         <message>
-            <location filename="behavior_with_feedback/behavior.xar" line="0"/>
             <source>Well done! Tadasana is an effective pose to improve your posture and relaxation. We continue with Virabhadrasana one, known as warrior one.</source>
             <comment>Text</comment>
-            <translation type="unfinished">Well done! Tadasana is an effective pose to improve your posture and relaxation. We continue with Virabhadrasana one, known as warrior one.</translation>
+            <translation type="obsolete">Well done! Tadasana is an effective pose to improve your posture and relaxation. We continue with Virabhadrasana one, known as warrior one.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_with_feedback/behavior.xar:/Ardha Uttanasana Reversed/Say</name>
+        <message>
+            <source>Well done! Tadasana is an effective pose to improve your posture and relaxation. We continue with Virabhadrasana one, known as warrior one.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Well done! Tadasana is an effective pose to improve your posture and relaxation. We continue with Virabhadrasana one, known as warrior one.</translation>
         </message>
     </context>
     <context>
@@ -98,10 +105,9 @@
     <context>
         <name>behavior_with_feedback/behavior.xar:/Dandasana (1)/Say</name>
         <message>
-            <location filename="behavior_with_feedback/behavior.xar" line="0"/>
             <source>Well done! Tadasana is an effective pose to improve your posture and relaxation. We continue with Virabhadrasana one, known as warrior one.</source>
             <comment>Text</comment>
-            <translation type="unfinished">Well done! Tadasana is an effective pose to improve your posture and relaxation. We continue with Virabhadrasana one, known as warrior one.</translation>
+            <translation type="obsolete">Well done! Tadasana is an effective pose to improve your posture and relaxation. We continue with Virabhadrasana one, known as warrior one.</translation>
         </message>
     </context>
     <context>
@@ -182,10 +188,9 @@
     <context>
         <name>behavior_with_feedback/behavior.xar:/Warrior I (1)/Say (2)</name>
         <message>
-            <location filename="behavior_with_feedback/behavior.xar" line="0"/>
             <source>Well done! Virabhadrasana one is a good pose to strengthen the legs, arms and shoulders and improve posture. We move on to Virabhadrasana two, known as warrior two.</source>
             <comment>Text</comment>
-            <translation type="unfinished">Well done! Virabhadrasana one is a good pose to strengthen the legs, arms and shoulders and improve posture. We move on to Virabhadrasana two, known as warrior two.</translation>
+            <translation type="obsolete">Well done! Virabhadrasana one is a good pose to strengthen the legs, arms and shoulders and improve posture. We move on to Virabhadrasana two, known as warrior two.</translation>
         </message>
     </context>
     <context>
@@ -208,28 +213,25 @@
     <context>
         <name>behavior_with_feedback/behavior.xar:/Warrior II (1)/Say (2)</name>
         <message>
-            <location filename="behavior_with_feedback/behavior.xar" line="0"/>
             <source>Well done! Virabhadrasana one is a good pose to strengthen the legs, arms and shoulders and improve posture. We move on to Virabhadrasana two, known as warrior two.</source>
             <comment>Text</comment>
-            <translation type="unfinished">Well done! Virabhadrasana one is a good pose to strengthen the legs, arms and shoulders and improve posture. We move on to Virabhadrasana two, known as warrior two.</translation>
+            <translation type="obsolete">Well done! Virabhadrasana one is a good pose to strengthen the legs, arms and shoulders and improve posture. We move on to Virabhadrasana two, known as warrior two.</translation>
         </message>
     </context>
     <context>
         <name>behavior_with_feedback/behavior.xar:/Warrior II (2)/Say</name>
         <message>
-            <location filename="behavior_with_feedback/behavior.xar" line="0"/>
             <source>Well done! Tadasana is an effective pose to improve your posture and relaxation. We continue with Virabhadrasana one, known as warrior one.</source>
             <comment>Text</comment>
-            <translation type="unfinished">Well done! Tadasana is an effective pose to improve your posture and relaxation. We continue with Virabhadrasana one, known as warrior one.</translation>
+            <translation type="obsolete">Well done! Tadasana is an effective pose to improve your posture and relaxation. We continue with Virabhadrasana one, known as warrior one.</translation>
         </message>
     </context>
     <context>
         <name>behavior_with_feedback/behavior.xar:/Warrior II Reversed/Say</name>
         <message>
-            <location filename="behavior_with_feedback/behavior.xar" line="0"/>
             <source>Well done! Tadasana is an effective pose to improve your posture and relaxation. We continue with Virabhadrasana one, known as warrior one.</source>
             <comment>Text</comment>
-            <translation type="unfinished">Well done! Tadasana is an effective pose to improve your posture and relaxation. We continue with Virabhadrasana one, known as warrior one.</translation>
+            <translation type="obsolete">Well done! Tadasana is an effective pose to improve your posture and relaxation. We continue with Virabhadrasana one, known as warrior one.</translation>
         </message>
     </context>
     <context>
